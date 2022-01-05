dc00bd0c5351f3b49ab34367b60dd648e6a9aefe
Naman Oli <namanoli111@gmail.com>
2022-01-05 13:29:59 +0530

    12 down

--- Goldman Sachs/12. Find total number of Squares in a NN chessboard.cpp
+++ Goldman Sachs/12. Find total number of Squares in a NN chessboard.cpp
@@ -1,1 +1,33 @@
 // https://www.geeksforgeeks.org/program-to-find-number-of-squares-on-a-chessboard/
+
+//https://practice.geeksforgeeks.org/problems/squares-in-nn-chessboard1801/1
+
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long N) {
+        // code here
+        return (N)*(N+1)*(2*N+1)/6;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
