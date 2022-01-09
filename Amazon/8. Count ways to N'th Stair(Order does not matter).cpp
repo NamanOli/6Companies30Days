6af3cd9178249c8c282cef4731a63a06372aa990
Naman Oli <namanoli111@gmail.com>
2022-01-10 01:30:30 +0530

    8 down

---
+++ Amazon/8. Count ways to N'th Stair(Order does not matter).cpp
@@ -0,0 +1,40 @@
+// https://practice.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1/#
+
+// https://www.geeksforgeeks.org/count-ways-reach-nth-stair/
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        // your code here
+        return (m/2)+1;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking count of testcases
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+}  // } Driver Code Ends
